6f5884af308ab65eed769b00231da046ee641488
Ubaldo Aviles <ubaldoaviles@Ubaldos-Laptop.local>
2024-01-17 17:47:44 -0600

    promesas3

--- promesas.js
+++ promesas.js
@@ -1,13 +1,25 @@
+//Promesas 3
 
-//uso de promesas
-let promesaExitosa = new Promise((resolve, reject) => {
-   //simula un error durante la ejecución
-    reject('La promesa fue rechazada');
-});
+const cargarDatosAPI = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error(
+            `Error en la solicitud ${response.status}: ${response.statusText}`
+          );
+        }
+      })
+      .then((data) => resolve(data))
+      .catch((error) => reject("Error en la solicitud: " + error.message));
+  });
+};
 
+const apiUrl = "https://fakestoreapi.com/products/";
 
-promesaExitosa.then((mensaje) => {
-    console.log(mensaje);
-}).catch((error) => {
-    console.error(error);
-});
+cargarDatosAPI(apiUrl)
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
