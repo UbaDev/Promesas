3919517c9f16fbc3952c8292b380057457262fa8
Ubaldo Aviles <ubaldoaviles@Ubaldos-Laptop.local>
2024-01-24 17:22:09 -0600

    fetch6

--- promesas.js
+++ promesas.js
@@ -1,13 +1,12 @@
-
-//uso de promesas
-let promesaExitosa = new Promise((resolve, reject) => {
-   //simula un error durante la ejecución
-    reject('La promesa fue rechazada');
-});
-
-
-promesaExitosa.then((mensaje) => {
-    console.log(mensaje);
-}).catch((error) => {
-    console.error(error);
-});
+fetch("https://fakestoreapi.com/products/1")
+  .then((res) => {
+    res.clone().json().then((data) => {
+        console.log(data);
+        })
+    res.clone().json().then((data) => {
+        console.log(data);
+        })
+         res.json().then((data) => {
+           console.log(data);
+         });
+  })
